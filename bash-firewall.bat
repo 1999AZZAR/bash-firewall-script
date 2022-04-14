37939502b87c0283c3426728a2f4eaed322f6828
azzar <azzar.mr.zs@gmail.com>
2022-04-15 01:44:55 +0700

    Create bash-firewall.bat

---
+++ bash-firewall.bat
@@ -0,0 +1,9 @@
+@echo off
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+for /R %%f in (*.exe) do (
+ netsh advfirewall firewall add rule name="Blocked: %%f" dir=out program="%%f" action=block
+ netsh advfirewall firewall add rule name="Blocked: %%f" dir=in program="%%f" action=block
+)
+pause
